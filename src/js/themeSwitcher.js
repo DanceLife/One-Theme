caab0a1f4fc3544c5272e0b066cb45e8b456f37b
Hector Higuera <hectorhiguera@outlook.com>
2018-09-07 16:16:00 -0400

    Edge Partial

--- src/js/themeSwitcher.js
+++ src/js/themeSwitcher.js
@@ -1,11 +1,13 @@
 function switchTheme(currentTheme){
         links = document.querySelectorAll('[rel=stylesheet]');
-    for(let i=0; i < links.length; i++){
+
+        for(let i=0; i < links.length; i++){
         let link = links[i];
-        link.disabled = true;
         let ref = link.href;
         if(ref.indexOf(currentTheme) > -1 || ref.indexOf("styles.css") > -1 || ref.indexOf("themerules.css") > -1){
             link.disabled = false;
+        }else{
+            link.disabled = true;
         }
     }
 }
